08744875726f585e3e6cb49b26c84b3f06e8635f
Mark Beale <bealebeaverst@gmail.com>
2024-07-16 05:03:08 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2);
+};
+function returnLastTwoDrivers() {
+  return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+function fareDoubler(fare){
+   return createFareMultiplier(2)(fare);
+}
+function fareTripler(fare) {
+  return createFareMultiplier(3)(fare);
+}
+function selectDifferentDrivers(arrayOfDrivers, driverSelector) {
+  return driverSelector(arrayOfDrivers);
+}
 // Code your solution in this file!
